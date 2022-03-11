83fa47b56ad8967cea3856344f7f78f94bb1dc7d
aerocyber <aerocyberdev@protonmail.com>
2022-03-11 04:29:32 +0000

    Add aurora commmands for Pyfile execution and writing.

--- src/aurora/aurora.py
+++ src/aurora/aurora.py
@@ -1,6 +1,8 @@
 import os
 from tomlkit import dumps
 from tomlkit import parse
+from shutil import copytree
+from subprocess import check_call
 
 
 class PyFile:
@@ -15,5 +17,35 @@ class PyFile:
         f = open(self.path)
         cmd = parse(f.read())
         f.close()
-        os.mkdir("build")
-        os.chdir(os.path.join(os.getcwd(), 'build'))
+        _dir = os.getcwd()
+        target_dir = os.path.abspath(cmd["DIRS"]["Build"])
+        src_dir = os.path.abspath(cmd["DIRS"]["Source"])
+        copytree(src_dir, target_dir)
+        os.chdir(target_dir)
+        command = cmd["Commands"]
+        commands = []
+        for key in command:
+            commands.insert(0, {key: command[key]})
+        command = {}
+        for i in commands:
+            check_call(commands[i].values().join("&&"))
+    
+
+class CreatePyFile:
+    def __init__(self, target_dir, overwrite = False):
+        if os.path.exists(os.path.join(target_dir, 'Pyfile')) and not overwrite:
+            raise FileExistsError(os.path.join(target_dir, 'Pyfile') + " exists and overwrite NOT allowed.")
+        self.path = os.path.join(target_dir, 'Pyfile')
+        self.cmds = {}
+
+
+    def commands(self, commands: dict):
+        self.cmds = self.cmds.update(commands)
+    
+
+    def exec(self):
+        if os.path.exists(self.path) and not overwrite:
+            raise FileExistsError(self.path + " exists and overwrite NOT allowed.")
+        f = open(self.path, 'w')
+        f.write(dumps(self.cmds))
+        f.close()
